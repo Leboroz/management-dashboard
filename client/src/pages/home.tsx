b57c0e63596705d0a2e97758c8376d43177b27eb
Leonardo Albornoz <albornozleo95@gmail.com>
2023-02-16 11:05:10 -0400

    Finish bar chart

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,53 @@
+import { useList } from '@pankod/refine-core'
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import { PieChart, PropertyCard, PropertyReferrals, TopAgent, TotalRevenue } from 'components';
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+          title="Properties for sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#475BE8', '#E4E8EF']}
+        />
+        <PieChart
+          title="Properties for sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#475BE8', '#E4E8EF']}
+        />
+        <PieChart
+          title="Properties for sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#475BE8', '#E4E8EF']}
+        />
+        <PieChart
+          title="Properties for sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#475BE8', '#E4E8EF']}
+        />
+      </Box>
+      <Stack
+        mt="25px"
+        width="100%"
+        direction={{
+          xs: 'column',
+          lg: 'row',
+        }}
+      >
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
+    </Box>
+  )
+};
+
+export default Home;
